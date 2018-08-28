ef0e004d69db50d8cb98982f93ee09ab72f33741
Gea-Suan Lin <gslin@gslin.org>
2018-08-29 03:32:40 +0800

    Add / test.

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 
+import bottle
 import ptt_api
+
+def test_index():
+    try:
+        ptt_api.index()
+    except bottle.HTTPResponse:
+        assert True
+        return
+
+    assert False
 
 def test_robotstxt():
     ptt_api.robotstxt()
