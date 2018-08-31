46d69617baac0c351244c609933a550e3dd2a53b
Gea-Suan Lin <gslin@gslin.org>
2018-09-01 04:34:53 +0800

    Fix after-script.

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo su - www-data -c "pip install -U -r /srv/ptt-api.hasname.com/requirements.txt"
+~/.pyenv/shims/pip install -U -r /srv/ptt-api.hasname.com/requirements.txt
 pkill -INT uwsgi
 LANG=en_US.UTF-8 ~/.pyenv/shims/uwsgi --ini /srv/ptt-api.hasname.com/uwsgi.ini > /dev/null 2>&1 &
