85be68c61eb83771fbf57f7809c15d5595e665dd
Gea-Suan Lin <gslin@gslin.org>
2018-09-02 01:50:39 +0800

    Change to use class-based test cases.

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -3,14 +3,15 @@
 import bottle
 import ptt_api
 
-def test_index():
-    try:
-        ptt_api.index()
-    except bottle.HTTPResponse:
-        assert True
-        return
+class smoke_test:
+    def test_index(self):
+        try:
+            ptt_api.index()
+        except bottle.HTTPResponse:
+            assert True
+            return
 
-    assert False
+        assert False
 
-def test_robotstxt():
-    ptt_api.robotstxt()
+    def test_robotstxt(self):
+        ptt_api.robotstxt()
